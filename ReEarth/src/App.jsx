5911e1b04dd4b12d981c1c6f1cc7d3249b29eb5a
Winoliya Ponsingh <win.sin.rt23@dypatil.edu>
2025-04-02 21:37:48 +0530

    Commit01

--- ReEarth/src/App.jsx
+++ ReEarth/src/App.jsx
@@ -16,6 +16,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ImageAnalyzer from "./components/ImageAnalyser";
 import ViewTrash from "./components/ViewTrash";
+import ViewNGO from "./components/ViewNGO";
 
 
 
@@ -36,6 +37,7 @@ const App = () => {
           <Route path="/view-trash" element={<ViewTrash />} />
           <Route path="/user-home" element={<UserHomePage />} />
           <Route path="/join-us" element={<JoinUs />} />
+          <Route path="/user-view-ngo" element={<ViewNGO />} />
 
 
 
